7260285839d6c4de55bc9ca1d3e87d840172cb5b
auubz <notpassword33>
2018-11-06 12:02:29 -0500

    Week 2 - class 1 completed, learned 6 new functions.

--- Week 2/Class 1/for function.py
+++ Week 2/Class 1/for function.py
@@ -1,5 +1,5 @@
-for y in range(0,11):
-    print (str(y) + ": ",end="")
+for y in range(11):
+    print(str(y) + ": ",end="")
     for x in range(y):
-    print("*",end="")
+        print("*",end="")
 print ()
